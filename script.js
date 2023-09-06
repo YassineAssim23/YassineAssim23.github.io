69592249fd08f4616e9a209aab72beae508226a4
YassineAssim23 <assimy@sheridancollege.ca>
2023-09-06 01:34:56 -0400

    Commented code

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+/**
+ * Function to toggle the mobile navigation menu open and closed.
+ */
 function toggleMenu() {
+    // Select the menu and icon elements
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    
+    // Toggle the "open" class on the menu and icon elements
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
+  
